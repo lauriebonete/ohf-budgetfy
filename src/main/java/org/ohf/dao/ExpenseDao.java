3c66d32d6095bf64de6d17103ba8785f629d6d87
kenji <nettanguilig@gmail.com>
2016-08-09 00:50:58 +0800

    Fixed refence links

---
+++ src/main/java/org/ohf/dao/ExpenseDao.java
@@ -0,0 +1,11 @@
+package org.ohf.dao;
+
+import org.evey.dao.BaseEntityDao;
+import org.ohf.bean.Expense;
+import org.ohf.bean.Program;
+
+/**
+ * Created by Laurie on 7/2/2016.
+ */
+public interface ExpenseDao extends BaseEntityDao<Expense,Long> {
+}
---
+++ src/main/java/org/ohf/service/ExpenseService.java
@@ -0,0 +1,11 @@
+package org.ohf.service;
+
+import org.evey.service.BaseCrudService;
+import org.ohf.bean.Expense;
+import org.ohf.bean.Program;
+
+/**
+ * Created by Laurie on 7/2/2016.
+ */
+public interface ExpenseService extends BaseCrudService<Expense> {
+}
